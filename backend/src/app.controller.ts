f9dbf3249e7cbe1b5e8cef11dd6a236245772c80
cbmja <143980455+cbmja@users.noreply.github.com>
2025-06-08 17:31:14 +0900

    - test / 이전으로 롤백

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import { AccessTokenGuard } from './auth/guards/access-token.guard';
+import { ApiBearerAuth } from '@nestjs/swagger'
 
 @Controller()
 export class AppController {
@@ -9,4 +12,18 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+
+
+
+  @Get('user-test')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth('access-token')
+  testUser(@Req() req: Request) {
+    console.log(req.user);
+    return 'test completed';
+  }
+
+
 }
---
+++ backend/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type JwtPayload = {
+  [key: string]: any;
+  iss?: string | undefined;
+  sub?: string | undefined;
+  aud?: string | string[] | undefined;
+  exp?: number | undefined;
+  nbf?: number | undefined;
+  iat?: number | undefined;
+  jti?: string | undefined;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access-token',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.AUTH_SECRET!,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return payload;
+  }
+}
